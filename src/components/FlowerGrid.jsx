e619ac18e6e093c8c5daa546463f266794974803
Thiva <thiva911@gmail.com>
2025-07-07 02:26:13 +0200

    Fix image paths and update components

--- src/components/FlowerGrid.jsx
+++ src/components/FlowerGrid.jsx
@@ -3,10 +3,10 @@ import FlowerCard from "./FlowerCard";
 import "./FlowerGrid.css";
 
 const bouquets = [
-  { src: "/DSC03285_(1).jpg", title: "Warm Wishes", rating: 4.5, price: 3000, oldPrice: 169 },
-  { src: "/DSC03295.jpg", title: "Caramel Charm", rating: 4.2, price: 4000, oldPrice: 199 },
-  { src: "/DSC03292.jpg", title: "Purple Hues", rating: 4.8, price: 2500, oldPrice: 149 },
-  { src: "/DSC03287.jpg", title: "Clouds of Affection", rating: 4.7, price: 5000 },
+  { src: `${process.env.PUBLIC_URL}/DSC03285_(1).jpg`, title: "Warm Wishes", rating: 4.5, price: 3000, oldPrice: 169 },
+  { src: `${process.env.PUBLIC_URL}/DSC03295.jpg`, title: "Caramel Charm", rating: 4.2, price: 4000, oldPrice: 199 },
+  { src: `${process.env.PUBLIC_URL}/DSC03292.jpg`, title: "Purple Hues", rating: 4.8, price: 2500, oldPrice: 149 },
+  { src: `${process.env.PUBLIC_URL}/DSC03287.jpg`, title: "Clouds of Affection", rating: 4.7, price: 5000 },
 ];
 
 function FlowerGrid() {
